b67f251ea6d03312a5403ce2e47858a8be7d197a
Mohd-kaleem125 <kaleemhussainans786@gmail.com>
2025-11-20 20:57:31 +0530

    Add files via upload

---
+++ Quiz-App/app.js
@@ -0,0 +1,96 @@
+const quizData = [{
+  question: "Which of the following is a client site language?",
+  a: "Java",
+  b: "C",
+  c: "Python",
+  d: "JavaScript",
+  correct: "d",
+},
+
+{
+  question: "What doest the HTML stand for?",
+  a: "Hypertext Markup language",
+  b: "Cascading style sheet",
+  c: "Jason object notation",
+  d: "Helicopters Terminals Motorboats Lamborginis",
+  correct: "a",
+},
+
+{
+  question: "What year was JavaScript launched?",
+  a: "1996",
+  b: "1995",
+  c: "1994",
+  d: "non of the above",
+  correct: "b",
+},
+
+{
+  question: "Whta does CSS stands for?",
+  a: "Hypertext Markup language",
+  b: "Cascading style sheet",
+  c: "Jason object Notation",
+  d: "Helicopters terminals Motorboats lamborginis",
+  correct: "b",
+},
+
+];
+
+let index = 0;
+let correct = 0;
+let incorrect = 0;
+let total = quizData.length;
+let questionBox = document.getElementById("questionBox");
+let allInputs = document.querySelectorAll("input[type= 'radio']");
+const loadQuestion = () => {
+  if(total === index){
+    return quizEnd();
+  }
+  reset()
+  const data = quizData[index];
+  questionBox.innerHTML = `${index+1}) ${data.question}`
+  allInputs[0].nextElementSibling.innerText = data.a
+  allInputs[1].nextElementSibling.innerText = data.b
+  allInputs[2].nextElementSibling.innerText = data.c
+  allInputs[3].nextElementSibling.innerText = data.d
+}
+
+document.querySelector("#submit").addEventListener("click",
+  function() {
+    const data = quizData[index]
+    const ans = getAnswer()
+    if(ans === data.correct){
+      correct++;
+    }else{
+      incorrect++;
+    }
+    index++;
+    loadQuestion()
+  }
+)
+
+const getAnswer = () => {
+  let ans;
+  allInputs.forEach((inputEl) => {
+    if(inputEl.checked){
+      ans = inputEl.value;
+    }
+  })
+  return ans;
+}
+
+const reset = () => {
+  allInputs.forEach((inputEl) => {
+    inputEl.checked = false;
+  })
+}
+
+
+const quizEnd = () => {
+  document.getElementsByClassName("container")[0].innerHTML= `<div class= "col">
+  <h3 class= "w-100">Thank you, You've scored ${correct} / ${total} </h3>
+  </div>
+  `
+}
+
+loadQuestion(index);
